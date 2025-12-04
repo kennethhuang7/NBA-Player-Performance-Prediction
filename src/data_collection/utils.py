144654841bcad9f4f838c8a0b32e9a9ac1cb31b3
Kenneth Huang <kennethhuangcs@gmail.com>
2025-12-04 02:47:52 -0500

    Fix database connection for GitHub Actions

--- src/data_collection/utils.py
+++ src/data_collection/utils.py
@@ -1,13 +1,21 @@
 import psycopg2
-from dotenv import load_dotenv
 import os
-import time
-import random
+from dotenv import load_dotenv
 
 load_dotenv()
 
 def get_db_connection():
-    database_url = os.getenv('DATABASE_URL')
+    if os.getenv('DATABASE_URL'):
+        database_url = os.getenv('DATABASE_URL')
+    else:
+        host = os.getenv('DB_HOST')
+        port = os.getenv('DB_PORT')
+        dbname = os.getenv('DB_NAME')
+        user = os.getenv('DB_USER')
+        password = os.getenv('DB_PASSWORD')
+        
+        database_url = f"postgresql://{user}:{password}@{host}:{port}/{dbname}"
+    
     return psycopg2.connect(database_url)
 
 def rate_limit(seconds=40.0):
